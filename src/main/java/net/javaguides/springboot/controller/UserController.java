2915fe88d7dacb17485be257ef63fc3e5474c56c
Sachin Gupta <sgs.engineer@yahoo.co.in>
2023-08-30 11:32:09 +0530

    Code Backup

---
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,118 @@
+package net.javaguides.springboot.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.exception.ErrorDetails;
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Tag(
+        name = "CRUD REST APIs for USER Resource",
+        description = "Create User, Update User, Get User, Get All Users, Delete User"
+)
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(
+            summary = "Create User Rest API",
+            description = "Create User Rest API is used to save user in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
+    @PostMapping
+    ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
+        UserDto savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @Operation(
+            summary = "Get User by Id Rest API",
+            description = "Get User by Id Rest API is used to get single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
+    @GetMapping("{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
+        UserDto user = userService.getUserById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @Operation(
+            summary = "Get all Users Rest API",
+            description = "Get all Users Rest API is used to get all the users from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @Operation(
+            summary = "Update User Rest API",
+            description = "Update User Rest API is used to update a particular user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @Valid @RequestBody UserDto userDto) {
+        userDto.setId(userId);
+        UserDto updateUser = userService.updateUser(userDto);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+
+    @Operation(
+            summary = "Delete User Rest API",
+            description = "Delete User Rest API is used to update a particular user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok("User Successfully deleted!");
+    }
+
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+//                                                                        WebRequest webRequest) {
+//        ErrorDetails errorDetails = new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(Boolean.FALSE),
+//                "USER_NOT_FOUND"
+//        );
+//
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+
+
+}
